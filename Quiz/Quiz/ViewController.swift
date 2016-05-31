5a9d3d6614a1abb491c913b5d289c0770001984d
Sydney Rossman-Reich <srossmanreich@gmail.com>
2016-05-31 01:37:31 -0400

    Setup skeleton for quiz app

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Sydney Rossman-Reich on 5/31/16.
+//  Copyright © 2016 Sydney Rossman-Reich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject){
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject){
+        
+    }
+
+
+}
+
